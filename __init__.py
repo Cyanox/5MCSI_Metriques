b870d876fe64616457286ecee4946e166d2e86bd
Cyanox <alexis.moyse@gmail.com>
2024-02-12 09:45:53 +0100

    Ad paris

--- __init__.py
+++ __init__.py
@@ -27,6 +27,14 @@ def meteo():
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
+@app.route("/histogramme/")
+def mongraphique():
+    return render_template("histogramme.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
