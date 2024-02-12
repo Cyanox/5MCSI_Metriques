bae5135dcc17414ca31a6f3e6e80fbf1fb912392
Cyanox <alexis.moyse@gmail.com>
2024-02-12 10:56:10 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	templates/contact.html

--- __init__.py
+++ __init__.py
@@ -4,26 +4,31 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-# Maj repo                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+# Maj repo
+app = Flask(__name__)
+
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
+
 
 @app.route("/contact/")
 def MaPremiereAPI():
     return render_template('contact.html')
 
+
 @app.route('/paris/')
 def meteo():
-    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
+    response = urlopen(
+        'https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c
+        temp_day_value = list_element.get('temp', {}).get('day') - 273.15  # Conversion de Kelvin en °c
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
@@ -32,10 +37,42 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
+
 @app.route("/histogramme/")
 def monhistogramme():
     return render_template("histogramme.html")
 
-  
+
+@app.route("/histogramme/")
+def monhistogramme():
+    return render_template("histogramme.html")
+
+
+@app.route('/commitsdata/')
+def commits():
+    response = urlopen(
+        'https://api.github.com/repos/Cyanox/5MCSI_Metriques/commits')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content:
+        dt_value = list_element.get('commit', {}).get('author', {}).get('date', {})
+        results.append({'Date': dt_value})
+    print(results)
+    return jsonify(results=results)
+
+
+@app.route("/commits/")
+def mescommits():
+    return render_template("commits.html")
+
+
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    minutes = date_object.minute
+    return jsonify({'minutes': minutes})
+
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
